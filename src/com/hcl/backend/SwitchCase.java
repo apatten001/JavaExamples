773e11eecb855f280b39a2b42ef2e612a894136e
Arnold Patten <apatten001@yahoo.com>
2022-02-01 16:49:26 -0500

    first commit

---
+++ src/com/hcl/backend/SwitchCase.java
@@ -0,0 +1,27 @@
+package com.hcl.backend;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		// giving an example of using switch and case 
+		
+		String timeOfDay = "Morning";
+		
+		switch(timeOfDay){
+		
+		 case  "Afternoon":
+			System.out.println("What's for Lunch?");
+		
+		 case "Dinner":
+			 System.out.println("What's for Dinner?");
+			
+	     default:
+	    	 System.out.println("Time for Breakfast!");
+			
+		
+		
+		
+		}
+	};
+
+}
